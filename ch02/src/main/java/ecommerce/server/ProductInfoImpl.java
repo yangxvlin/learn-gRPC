64af76a22ab835f7f75e783819f506e76a9317a9
xulinyang <xulinyang@ea.com>
2022-01-28 12:10:59 +0800

    add comments

--- ch02/src/main/java/ecommerce/server/ProductInfoImpl.java
+++ ch02/src/main/java/ecommerce/server/ProductInfoImpl.java
@@ -11,14 +11,14 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Logger;
 
-public class ProductInfoImpl extends ProductInfoGrpc.ProductInfoImplBase {
+public class ProductInfoImpl extends ProductInfoGrpc.ProductInfoImplBase { // extend generated service interface
 
     private Map productMap = new HashMap<String, ProtoInfo.Product>();
     private static final Logger logger = Logger.getLogger(ProductInfoClient.class.getName());
 
     @Override
     public void addProduct(ProtoInfo.Product request,
-                           io.grpc.stub.StreamObserver<ProtoInfo.ProductID> responseObserver) {
+                           io.grpc.stub.StreamObserver<ProtoInfo.ProductID> responseObserver) { // ProtoInfo contains the generated message class (Product, ProductID)
         UUID uuid = UUID.randomUUID();
         String randomUUIDString = uuid.toString();
         request = request.toBuilder().setId(randomUUIDString).build();
@@ -26,8 +26,8 @@ public class ProductInfoImpl extends ProductInfoGrpc.ProductInfoImplBase {
         ProtoInfo.ProductID id
                 = ProtoInfo.ProductID.newBuilder().setValue(randomUUIDString).build();
         logger.info("addProduct");
-        responseObserver.onNext(id);
-        responseObserver.onCompleted();
+        responseObserver.onNext(id); // Send a response back to the client.
+        responseObserver.onCompleted(); // End the client call by closing the stream.
     }
 
     @Override
@@ -36,10 +36,10 @@ public class ProductInfoImpl extends ProductInfoGrpc.ProductInfoImplBase {
         String id = request.getValue();
         logger.info("getProduct");
         if (productMap.containsKey(id)) {
-            responseObserver.onNext((ProtoInfo.Product) productMap.get(id));
-            responseObserver.onCompleted();
+            responseObserver.onNext((ProtoInfo.Product) productMap.get(id)); // Send a response back to the client so that client can stub.getValue()
+            responseObserver.onCompleted(); // End the client call by closing the stream.
         } else {
-            responseObserver.onError(new StatusException(Status.NOT_FOUND));
+            responseObserver.onError(new StatusException(Status.NOT_FOUND)); // Send an error back to the client.
         }
     }
 }
